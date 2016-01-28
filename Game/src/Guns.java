29a033fed3dae513f497931ac4ab8937a58502b4
jakob.fettig <jakob.fettig@MU-C1-12.mullen.pvt.k12.co.us>
2016-01-28 13:40:50 -0700

    initial commit

---
+++ Game/src/Guns.java
@@ -0,0 +1,45 @@
+
+public class Guns
+	{
+		private String Name, set;
+		private int Ammunation, Damage;
+		public void Gun (String col, String cl, int a, int buns)
+			{
+				Name = col;
+				set = cl;
+				Ammunation = a;
+				Damage = buns;
+			}
+		public String getName()
+			{
+				return Name;
+			}
+		public void setName(String name)
+			{
+				Name = name;
+			}
+		public String getset()
+			{
+				return set;
+			}
+		public void setset(String class1)
+			{
+				set = class1;
+			}
+		public int getAmmunation()
+			{
+				return Ammunation;
+			}
+		public void setAmmunation(int ammunation)
+			{
+				Ammunation = ammunation;
+			}
+		public int getDamage()
+			{
+				return Damage;
+			}
+		public void setDamage(int damage)
+			{
+				Damage = damage;
+			}
+	}
---
+++ Game/src/Player.java
@@ -0,0 +1,17 @@
+
+public class Player
+	{
+
+		public static void main(String[] args)
+			{
+				private String Name;				
+				private int Health, Armor;
+				public void Player (String col, int a, int buns)
+					{
+						Name = col;						
+						Health = a;
+						Armor = buns;
+					}
+			}
+
+	}
